23850a895e630ec46362040ff8015afe27392611
Natasha Chiu <chiuchiu07@gmail.com>
2023-07-25 03:01:09 +0800

    create and populate assignments and assignment_submissions

---
+++ migrations/assignments_submissions.sql
@@ -0,0 +1,19 @@
+DROP TABLE assignment_submissions CASCADE;
+DROP TABLE assignments CASCADE;
+
+CREATE TABLE assignments (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  content TEXT,
+  day INTEGER,
+  chapter INTEGER,
+  duration INTEGER
+);
+
+CREATE TABLE assignment_submissions (
+  id SERIAL PRIMARY KEY,
+  assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
+  student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
+  duration INTEGER,
+  submission_date DATE
+);
